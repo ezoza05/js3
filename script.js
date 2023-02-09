bf71e39b43197a96da777c62b8c805d7c7dd1196
ezoza05 <101642184+ezoza05@users.noreply.github.com>
2023-02-09 23:28:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var ask = confirm('test for my friends');
+if (ask){
+    var ask1 = confirm(' Are u ready for the test?');
+    if(ask1){
+          alert('lets do it')
+          alert('FIRST question')
+         var test = prompt("do u luv me? (u can say yes or no) ");
+         if ( test == 'yes'){
+             alert('i luv u too:3');
+             alert('test is over bc...')
+             alert('i luv u and i dont test our friendship')
+             alert(' u mean a lot to me<3')
+             alert('thank U for being my bsf')
+         }
+         else if( test == 'no'){
+             alert("SADDD:( but i luv u" );
+         }
+          alert(' now tap the link below')
+    }
+    else{
+        alert('i believe in u, u can do it!<3')
+        alert('FIRST question')
+        var test = prompt('do u luv me? ');
+        if ( test == 'yes'){
+            alert('i luv u too:3');
+            
+        alert(' u mean a lot to me<3')
+        alert('thank u for U for being my bsf')
+        alert(' now tap the link below')
+        }
+        else if( test == 'no'){
+            alert(" but i still luv u! :)");
+        }
+    
+
+        alert(' now tap the link below')
+    }
+  
+}
+ else{
+     alert(' u still take the test!')
+     alert('why?')
+     alert('if i sent this to u, it means i consider u as a friend!')
+     alert('FIRST question')
+     var test = prompt("do u luv me? (u can say yes or no) ");
+     if ( test == 'yes'){
+         alert('i luv u too:3');
+         alert('test is over bc...')
+         alert('i luv u and i dont test our friendship')
+         alert(' u mean a lot to me<3')
+         alert('thank u for U for being my bsf')
+     }
+     else if( test == 'no'){
+         alert("SADDD:( but i luv u" );
+     }
+      alert(' now tap the link below')
+     
+ }
+ /*var year = +prompt('введите год рождения')
+
+ if(isNaN(year)){
+     alert('pls write w letters...');
+ }
+ else if(year >= 2022){
+     alert("u haven't born yet dude");
+ }
+ else if(year < 1900){
+      alert('u r too old for this world');
+ }
+ else if( year>= 1900 && year <2022){
+     alert('now u r ' +(2022- year));
+ }*/
